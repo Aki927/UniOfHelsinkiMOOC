b699dc65a032fdad99edde1cc118f1bcd49a88b5
Aki Corpuz <akirucorpuz@Akis-MacBook-Air.local>
2023-08-22 13:23:22 -0400

    completed part 4 expercise 27

--- part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -6,12 +6,32 @@ public class IsItInTheFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Name of the file:");
+
         String file = scanner.nextLine();
+        boolean found = false;
+        String searchedFor;
+        String row;
 
-        System.out.println("Search for:");
-        String searchedFor = scanner.nextLine();
+        try ( Scanner fileScan = new Scanner(Paths.get(file))) {
 
+            System.out.println("Search for:");
+            searchedFor = scanner.nextLine();
+
+            while (fileScan.hasNextLine()) {
+                row = fileScan.nextLine();
+                if (row.contains(searchedFor)) {
+                    found = true;
+                    break; // Exit loop as soon as the text is found.
+                }
+            }
+            if (found) {
+                System.out.println("Found!");
+            } else {
+                System.out.println("Not found.");
+            }
+        } catch (Exception ex) {
+            System.out.println("Reading the file " + file + " failed.");
+        }
     }
 }
