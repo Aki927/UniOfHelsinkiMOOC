bd0e7cfe0da1d130fd2143f4261b2d63ae7b4c72
Aki Corpuz <akirucorpuz@Akis-MacBook-Air.local>
2023-08-22 14:27:20 -0400

    completed part 4 exercise 28

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -13,7 +13,27 @@ public class NumbersFromAFile {
         int lowerBound = Integer.valueOf(scanner.nextLine());
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
+        System.out.println("Numbers: " + 
+                countNumInRange(file, lowerBound, upperBound));
 
+    }
+    
+    public static int countNumInRange(String file, int lowerBound, int upperBound){
+        String row;
+        int numCounter = 0;
+        
+        try ( Scanner fileScan = new Scanner(Paths.get(file))) {                 
+            while (fileScan.hasNextLine()) {
+                row = fileScan.nextLine();
+                int number = Integer.parseInt(row);
+                if (number >= lowerBound && number <= upperBound) {
+                    numCounter++;
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex);
+        }
+        return numCounter;
     }
 
 }
