ba99528cdf0afc64a41129a992436e5d0b301823
Aki Corpuz <akirucorpuz@Akis-MacBook-Air.local>
2023-08-22 17:46:06 -0400

    completed part 4 exercise 30

--- part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
+++ part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
@@ -22,10 +22,20 @@ public class StoringRecords {
 
     public static ArrayList<Person> readRecordsFromFile(String file) {
         ArrayList<Person> persons = new ArrayList<>();
-
-        // Write here the code for reading from file
-        // and printing the read records
+        
+        try (Scanner fileScan = new Scanner(Paths.get(file))) {
+            while (fileScan.hasNextLine()) {
+                String line = fileScan.nextLine();
+                
+                String[] pieces = line.split(",");
+                String name = pieces[0];
+                int age = Integer.valueOf(pieces[1]);
+                
+                persons.add(new Person(name, age));
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
         return persons;
-
     }
 }
